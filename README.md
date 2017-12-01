ba8560cec82703262e73306d2c91e60f4543a654
Nguyễn Thái Sơn <leoofcoltech@gmail.com>
2017-12-01 15:50:45 +0100

    part2

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ $ ./configure-tun.sh
 
 The script `configure-tun.sh` will ask you to enter the device name and it will config the device with
 IP v6: `fc00:1234:ffff::1/64`
+
+
+## Updated 1 Dec
+
+Now you don't need to run `configure-tun.sh` to config device IP. Everything will
+be done in the code
--- partage/iftun.py
+++ partage/iftun.py
@@ -2,6 +2,8 @@ import fcntl
 import os
 import struct
 import sys
+import subprocess
+from array import array
 
 # Some constants used to ioctl the device file
 TUNSETIFF = 0x400454ca
@@ -17,10 +19,8 @@ def create_tun_dev(dev_name):
     ifr = struct.pack('16sH', dev_name, IFF_TUN | IFF_NO_PI)
     fcntl.ioctl(tun, TUNSETIFF, ifr)
     fcntl.ioctl(tun, TUNSETOWNER, 1000)
-
-    # Bring it up and assign addresses.
-    # /sbin/ifconfig tun0 inet6 add fc00:1234:ffff::1/64 
-    # subprocess.check_call('ifconfig tun0 192.168.7.1 pointopoint 192.168.7.2 up', shell=True)
+    subprocess.check_call('/sbin/ifconfig %s inet6 add fc00:1234:ffff::1/64 up' % dev_name,
+                          shell=True)
     return tun
 
 
@@ -36,8 +36,7 @@ if __name__ == "__main__":
 
     while True:
         # Read an IP packet been sent to this TUN device.
-        print ("In loop")
-        packet = list(os.read(tun.fileno(), 2048))
+        packet = array('B', os.read(tun.fileno(), 2048))
 
         # Modify it to an ICMP Echo Reply packet.
         #
@@ -45,11 +44,7 @@ if __name__ == "__main__":
         # been sent to our TUN device as an ICMP Echo Request.
 
         # Swap source and destination address.
-        packet[12:16], packet[16:20] = packet[16:20], packet[12:16]
+        # packet[12:16], packet[16:20] = packet[16:20], packet[12:16]
 
-        # Under Linux, the code below is not necessary to make the TUN device to
-        # work. I don't know why yet. If you run tcpdump, you can see the
-        # difference.
         # Write the reply packet into TUN device.
-        print (''.join(packet))
-        os.write(1, ''.join(packet))
+        os.write(1, bytes(packet))
